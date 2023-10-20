7d06dcf1c44ac521e3f33f4a5817453bbe87aff8
서원준 <ncprog1@gmail.com>
2023-10-20 15:04:19 +0900

    interface

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,26 @@
 "use strict";
-let user1;
-user1 = {
-    name: 'Max',
-    age: 10,
-    greet(phrase) {
-        console.log(phrase + ' ' + this.name);
-    }
+let add;
+add = (n1, n2) => {
+    return n1 + n2;
 };
+let user1;
+class Person {
+    constructor(n) {
+        this.age = 10;
+        if (n) {
+            this.name = n;
+        }
+    }
+    greet(phrase) {
+        if (this.name) {
+            console.log(phrase + ' ' + this.name);
+        }
+        else {
+            console.log('Hi');
+        }
+    }
+}
+user1 = new Person();
+console.log(user1);
 user1.greet('Hi there - I am');
 //# sourceMappingURL=app.js.map
